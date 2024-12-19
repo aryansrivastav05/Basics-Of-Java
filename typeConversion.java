d610d8bc592584b9b13434a48af5ed44b585dc5b
Aryan Sriastav <srivastavaryan182@gmail.com>
2024-12-19 12:24:03 +0530

    Type conversion

---
+++ typeConversion.java
@@ -0,0 +1,9 @@
+public class typeConversion {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch2 = 'b';
+        int number = ch;
+        int num2 = ch2;
+        System.out.println(number);
+    }
+}
---
+++ typePromotion.java
@@ -0,0 +1,8 @@
+public class typePromotion {
+    public static void main(String[] args) {
+        char ch = 'a';
+        char ch2 = 'b';
+       
+        System.out.println(ch - ch2);
+    }
+}
