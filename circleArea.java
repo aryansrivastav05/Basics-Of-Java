24d625ce21420bb8dba527ee92463dd20b1f1b3a
Aryan Sriastav <srivastavaryan182@gmail.com>
2024-12-19 12:15:08 +0530

    area of circle

---
+++ circleArea.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class circleArea{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of cirle");
+        double r = sc.nextFloat();
+        double area = (3.14)*r*r;
+        System.out.println("Area of cirle = "+area);
+
+
+    }
+}
