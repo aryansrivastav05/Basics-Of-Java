50e9b960d5752b3722f3fa55dad39c6ec4cd2994
Aryan Sriastav <srivastavaryan182@gmail.com>
2025-02-20 12:01:25 +0530

    whileLoop

---
+++ printNum.java
@@ -0,0 +1,10 @@
+public class printNum {
+    public static void main(String[] args) {
+        int count  = 0;
+        while(count<=10)
+        {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
